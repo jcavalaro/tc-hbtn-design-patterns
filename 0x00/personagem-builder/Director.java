e445cf6440e63cc9b6e3969da48ada0ee33cc70c
jcavalaro <jcavalaro@ciandt.com>
2022-07-19 16:54:10 -0300

    design Patterns

---
+++ 0x00/personagem-builder/Director.java
@@ -0,0 +1,28 @@
+public class Director {
+
+    void buildGuerreiro(Builder builder){
+        builder.setNome("Guerreiro");
+        builder.setInteligencia(1);
+        builder.setForca(8);
+        builder.setVigor(5);
+        builder.setResistencia(10);
+        builder.setDestreza(6);
+    }
+    void buildLadrao(Builder builder){
+        builder.setNome("Ladrao");
+        builder.setInteligencia(2);
+        builder.setForca(6);
+        builder.setVigor(8);
+        builder.setResistencia(5);
+        builder.setDestreza(10);
+    }
+    void buildMago(Builder builder){
+        builder.setNome("Mago");
+        builder.setInteligencia(10);
+        builder.setForca(2);
+        builder.setDestreza(4);
+        builder.setVigor(5);
+        builder.setResistencia(3);
+    }
+
+}
---
+++ 0x00/personagem-builder/Personagem.java
@@ -0,0 +1,99 @@
+import java.util.Objects;
+
+public abstract class Personagem {
+
+    private String nome;
+
+    private TipoPersonagem tipo;
+
+    private int inteligencia, forca, vigor, resistencia, destreza;
+
+    public Personagem(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+    }
+
+    public abstract double getDanoAtaque();
+
+    @Override
+    public String toString() {
+        return String.format("Personagem { nome = %s, tipo = %s, inteligencia = %d, forca = %d, vigor = %d, resistencia = %d, destreza = %d, dano ataque = %.2f }", getNome(), getTipo(), getInteligencia(), getForca(), getVigor(), getResistencia(),getDestreza(), getDanoAtaque());
+    }
+
+    public Personagem(String nome, TipoPersonagem tipo, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.inteligencia = inteligencia;
+        this.forca = forca;
+        this.vigor = vigor;
+        this.resistencia = resistencia;
+        this.destreza = destreza;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personagem that = (Personagem) o;
+        return inteligencia == that.inteligencia && forca == that.forca && vigor == that.vigor && resistencia == that.resistencia && destreza == that.destreza && Objects.equals(nome, that.nome) && tipo == that.tipo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, tipo, inteligencia, forca, vigor, resistencia, destreza);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoPersonagem getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getVigor() {
+        return vigor;
+    }
+
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+}
